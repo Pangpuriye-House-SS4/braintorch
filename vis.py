57431a864d4aa732361b12f8c211be22ff9fb859
monchinawat <monshinawatra@gmail.com>
2024-05-16 22:37:13 +0700

    Edit visualize channels

--- vis.py
+++ vis.py
@@ -5,7 +5,7 @@ import numpy as np
 def visualtize_signals(signal: np.ndarray, distance: int = 20, figsize=(12, 26), lines=None):
     n_channels = signal.shape[1]
     plt.figure(figsize=figsize)
-    plt.subplots(n_channels, 1, 1)
+    plt.subplot(n_channels, 1, 1)
 
     for channel in range(8):
         segment = signal[:, channel]
