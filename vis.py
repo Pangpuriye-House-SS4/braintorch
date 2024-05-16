f805daa9cc897bd0dfbfb150c013046aa39be973
monchinawat <monshinawatra@gmail.com>
2024-05-16 22:33:10 +0700

    Fix visualize error

--- vis.py
+++ vis.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def visualtize_signals(signal: np.ndarray, figsize=(12, 26)):
+def visualtize_signals(signal: np.ndarray, figsize=(12, 26), lines=None):
     n_channels = signal.shape[1]
     fig, axs = plt.subplots(n_channels, 1, figsize=figsize)
 
     for channel in range(8):
         segment = signal[:, channel]
-        lines = np.linspace(range(segment.shape[0]))
-        axs[channel](lines, segment, label=channel)
+        if lines == None:
+            lines = range(len(segment))
+
+        axs[channel].plot(lines, segment, label=channel)
 
     plt.show()
